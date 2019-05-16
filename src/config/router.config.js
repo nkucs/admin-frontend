8a19e0510568d0f929ce4922accdeb8f0dd51b6f
NeeXxx <neexgzyx@163.com>
2019-05-16 13:10:59 +0800

    First commit.

---
+++ src/config/router.config.js
@@ -0,0 +1,85 @@
+// eslint-disable-next-line
+import { UserLayout, BasicLayout, RouteView, BlankLayout, PageView } from '@/components/layouts'
+
+export const asyncRouterMap = [
+
+  {
+    path: '/',
+    name: 'index',
+    component: BasicLayout,
+    meta: { title: '首页' },
+    redirect: '/problem',
+    children: [
+      {
+        path: '/problem',
+        name: 'problem',
+        component: () => import('@/views/result/Success'),
+        meta: { title: '题目管理', icon: 'dashboard', permission: [ 'dashboard' ] }
+      },
+      {
+        path: 'course',
+        name: 'course',
+        component: () => import('@/views/result/Success'),
+        meta: { title: '课程管理', icon: 'dashboard', permission: [ 'dashboard' ] }
+      },
+      {
+        path: '/user/',
+        name: 'user',
+        component: () => import('@/views/result/Success'),
+        meta: { title: '用户管理', icon: 'dashboard', permission: [ 'dashboard' ] }
+      }
+    ]
+  },
+  {
+    path: '*', redirect: '/404', hidden: true
+  }
+]
+
+/**
+ * 基础路由
+ * @type { *[] }
+ */
+export const constantRouterMap = [
+  {
+    path: '/user',
+    component: UserLayout,
+    redirect: '/user/login',
+    hidden: true,
+    children: [
+      {
+        path: 'login',
+        name: 'login',
+        component: () => import(/* webpackChunkName: "user" */ '@/views/user/Login')
+      },
+      {
+        path: 'register',
+        name: 'register',
+        component: () => import(/* webpackChunkName: "user" */ '@/views/user/Register')
+      },
+      {
+        path: 'register-result',
+        name: 'registerResult',
+        component: () => import(/* webpackChunkName: "user" */ '@/views/user/RegisterResult')
+      }
+    ]
+  },
+
+  {
+    path: '/test',
+    component: BlankLayout,
+    redirect: '/test/home',
+    children: [
+      {
+        path: 'home',
+        name: 'TestHome',
+        component: () => import('@/views/Home')
+      }
+    ]
+  },
+
+  {
+    path: '/404',
+    component: () => import(/* webpackChunkName: "fail" */ '@/views/exception/404')
+  },
+
+]
