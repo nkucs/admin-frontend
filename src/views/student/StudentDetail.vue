bcea611b7f4a6c533dc1f2e131928195a5ba56dc
root <root@RODEVEN.localdomain>
2019-05-26 17:39:26 +0800

    style: format the code

--- src/views/student/StudentDetail.vue
+++ src/views/student/StudentDetail.vue
@@ -5,57 +5,39 @@
         <img :src="imgUrl">
       </div>
       <a-row>
-        <p>
-          学生ID&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{{ id }}
-        </p>
+        <p>学生ID&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{{ id }}</p>
       </a-row>
       <a-row>
-        <p>
-          性别&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{{ sex }}
-        </p>
+        <p>性别&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{{ sex }}</p>
       </a-row>
       <a-row>
-        <p>
-          编号&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{{ number }}
-        </p>
+        <p>编号&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{{ number }}</p>
       </a-row>
       <a-row>
-        <p>
-          账号&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{{ account }}
-        </p>
+        <p>账号&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{{ account }}</p>
       </a-row>
       <a-row>
-        <p>
-          昵称&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{{ nickname }}
-        </p>
+        <p>昵称&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{{ nickname }}</p>
       </a-row>
       <a-row>
-        <p>
-          状态&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{{ state }}
-        </p>
+        <p>状态&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{{ state }}</p>
       </a-row>
       <a-row>
-        <p>
-          班级&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{{ class_number }}
-        </p>
+        <p>班级&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{{ class_number }}</p>
       </a-row>
       <a-row>
-        <p>
-          寝室&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{{ dormitory }}
-        </p>
+        <p>寝室&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{{ dormitory }}</p>
       </a-row>
       <a-row>
-        <p>
-          省份&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{{ province }}
-        </p>
+        <p>省份&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{{ province }}</p>
       </a-row>
     </a-card>
     <a-row style="margin-top: 20px;">
-      <a-col :span=20></a-col>
-      <a-col :span=2>
+      <a-col :span="20"></a-col>
+      <a-col :span="2">
         <a-button type="primary" @click="resetPwd">重置密码</a-button>
       </a-col>
-      <a-col :span=2>
+      <a-col :span="2">
         <a-button class="modify" type="primary" @click="resetInfo">修改信息</a-button>
       </a-col>
     </a-row>
@@ -63,62 +45,62 @@
 </template>
 
 <script>
-  export default {
-    name: 'StudentDetail',
-    data(){
+export default {
+  name: 'StudentDetail',
+  data() {
     return {
-      imgUrl:require('../../../public/avatar2.jpg')
-      }
+      imgUrl: require('../../../public/avatar2.jpg')
+    }
+  },
+  props: {
+    id: {
+      type: String,
+      default: '0'
     },
-    props: {
-      id: {
-        type: String,
-        default: '0'
-      },
-      sex: {
-        type: String,
-        default: '男'
-      },
-      number: {
-        type: String,
-        default: '0'
-      },
-      account: {
-        type: String,
-        default: '0'
-      },
-      nickname: {
-        type: String,
-        default: '小明'
-      },
-      state: {
-        type: String,
-        default: '正常'
-      },
-      classNumber: {
-        type: String,
-        default: '2班'
-      },
-      dormitory: {
-        type: String,
-        default: '429'
-      },
-      province: {
-        type: String,
-        default: '山东'
-      }
+    sex: {
+      type: String,
+      default: '男'
     },
-    methods:{
-      resetPwd (account) {
-        this.account=account
-        console.log('Received Account: ', account)
-      },
-      resetInfo (account){
-        this.account=account
-        console.log('Received Account: ', account)
-      },
+    number: {
+      type: String,
+      default: '0'
     },
+    account: {
+      type: String,
+      default: '0'
+    },
+    nickname: {
+      type: String,
+      default: '小明'
+    },
+    state: {
+      type: String,
+      default: '正常'
+    },
+    classNumber: {
+      type: String,
+      default: '2班'
+    },
+    dormitory: {
+      type: String,
+      default: '429'
+    },
+    province: {
+      type: String,
+      default: '山东'
+    }
+  },
+  methods: {
+    resetPwd(account) {
+      this.account = account
+      console.log('Received Account: ', account)
+    },
+    resetInfo(account) {
+      this.account = account
+      console.log('Received Account: ', account)
+    }
   }
+}
 </script>
 
 <style lang="scss" scoped>
--- src/views/student/StudentList.vue
+++ src/views/student/StudentList.vue
@@ -3,24 +3,19 @@
     <template>
       <div style="margin-bottom: 16px">
         ID:
-        <a-input style="width: 10%; margin-right: 5%;" placeholder="请输入"/>
-        编号:
-        <a-input style="width: 10%; margin-right: 5%;" placeholder="请输入"/>
-        账号：
-        <a-input style="width: 10%; margin-right: 5%;" placeholder="请输入"/>
-        状态：
-        <a-select defaultValue="normal" >
+        <a-input style="width: 10%; margin-right: 5%;" placeholder="请输入"/>编号:
+        <a-input style="width: 10%; margin-right: 5%;" placeholder="请输入"/>账号：
+        <a-input style="width: 10%; margin-right: 5%;" placeholder="请输入"/>状态：
+        <a-select defaultValue="normal">
           <a-select-option value="normal">正常</a-select-option>
           <a-select-option value="abnormal">异常</a-select-option>
           <a-select-option value="closed">关闭</a-select-option>
-        </a-select>
-        &nbsp&nbsp
+        </a-select>&nbsp&nbsp
         性别:
         <a-select defaultValue="male">
           <a-select-option value="male">男</a-select-option>
           <a-select-option value="female">女</a-select-option>
-        </a-select>
-        &nbsp&nbsp
+        </a-select>&nbsp&nbsp
         <a-button type="primary" style="margin-right: 2%;" @click="inquire">查询</a-button>
         <a-button>取消</a-button>
       </div>
@@ -28,36 +23,23 @@
     <template>
       <div>
         <div style="margin-bottom: 16px">
-          <a-button
-            style="margin-right: 2%;"
-            type="primary"
-          >
-            新建用户
-          </a-button>
-          <a-button
-            style="margin-right: 2%;"
-          >
-            批量导入
-          </a-button>
-          <a-button
-            @click="start"
-            :disabled="!hasSelected"
-            :loading="loading"
-          >
-            批量操作
-          </a-button>
+          <a-button style="margin-right: 2%;" type="primary">新建用户</a-button>
+          <a-button style="margin-right: 2%;">批量导入</a-button>
+          <a-button @click="start" :disabled="!hasSelected" :loading="loading">批量操作</a-button>
           <span style="margin-left: 8px">
-            <template v-if="hasSelected">
-              {{ `已选择 ${selectedRowKeys.length} 项` }}
-            </template>
+            <template v-if="hasSelected">{{ `已选择 ${selectedRowKeys.length} 项` }}</template>
           </span>
         </div>
-        <a-table :rowSelection="{selectedRowKeys: selectedRowKeys, onChange: onSelectChange}" :columns="columns" :dataSource="data">
+        <a-table
+          :rowSelection="{selectedRowKeys: selectedRowKeys, onChange: onSelectChange}"
+          :columns="columns"
+          :dataSource="data"
+        >
           <span slot="action" slot-scope="text, record">
             <a href="javascript:;">详情</a>
-            <a-divider type="vertical" />
+            <a-divider type="vertical"/>
             <a href="javascript:;">修改</a>
-            <a-divider type="vertical" />
+            <a-divider type="vertical"/>
             <a href="javascript:;">删除</a>
           </span>
         </a-table>
@@ -66,36 +48,42 @@
   </a-card>
 </template>
 <script>
-const columns = [{
-  title: 'ID',
-  dataIndex: 'ID',
-}, {
-  title: '编号',
-  dataIndex: 'number',
-}, {
-  title: '账号',
-  dataIndex: 'account',
-}, {
-  title: '状态',
-  dataIndex: 'state',
-}, {
-  title: '性别',
-  dataIndex: 'gender',
-}, {
-  title: 'Action',
-  key: 'action',
-  scopedSlots: { customRender: 'action' },
-}]
+const columns = [
+  {
+    title: 'ID',
+    dataIndex: 'ID'
+  },
+  {
+    title: '编号',
+    dataIndex: 'number'
+  },
+  {
+    title: '账号',
+    dataIndex: 'account'
+  },
+  {
+    title: '状态',
+    dataIndex: 'state'
+  },
+  {
+    title: '性别',
+    dataIndex: 'gender'
+  },
+  {
+    title: 'Action',
+    key: 'action',
+    scopedSlots: { customRender: 'action' }
+  }
+]
 
 const data = []
 for (let i = 0; i < 30; i++) {
   data.push({
-    ID: i+1,
+    ID: i + 1,
     number: `abd`,
-    account: 10000+i,
+    account: 10000 + i,
     state: `关闭`,
-    gender: '男',
-    
+    gender: '男'
   })
 }
 
@@ -105,7 +93,7 @@ export default {
       data,
       columns,
       selectedRowKeys: [], // Check here to configure the default column
-      loading: false,
+      loading: false
     }
   },
   computed: {
@@ -114,7 +102,7 @@ export default {
     }
   },
   methods: {
-    start () {
+    start() {
       this.loading = true
       // ajax request after empty completing
       setTimeout(() => {
@@ -122,22 +110,22 @@ export default {
         this.selectedRowKeys = []
       }, 1000)
     },
-    onSelectChange (selectedRowKeys) {
+    onSelectChange(selectedRowKeys) {
       console.log('selectedRowKeys changed: ', selectedRowKeys)
       this.selectedRowKeys = selectedRowKeys
     },
-    inquire (ID,number,account,state,gender) {
-      this.ID=ID
-      this.account=account
-      this.number=number
-      this.state=state
-      this.gender=gender
+    inquire(ID, number, account, state, gender) {
+      this.ID = ID
+      this.account = account
+      this.number = number
+      this.state = state
+      this.gender = gender
       console.log('Received ID: ', ID)
       console.log('Received Account: ', account)
       console.log('Received Number: ', number)
       console.log('Received State: ', state)
       console.log('Received Gender: ', gender)
-    },
-  },
+    }
+  }
 }
 </script>
