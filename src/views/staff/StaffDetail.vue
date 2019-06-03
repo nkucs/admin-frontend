40ec83cde54f24b86c6139f03d3e93b5ea49ec10
DanielK <wx_bb92dd8337ab4fb387723a2022d2fc42@git.code.tencent.com>
2019-06-03 18:24:38 +0800

    feat:add_staffdetail

--- src/views/staff/StaffDetail.vue
+++ src/views/staff/StaffDetail.vue
@@ -39,41 +39,93 @@ function getBase64(img, callback) {
   reader.addEventListener('load', () => callback(reader.result))
   reader.readAsDataURL(img)
 }
+import store from '../../store/stafflist.js'
+import { error } from 'util';
 export default {
   name: 'StaffCreation',
-  props: {
-    id: {
-      type: String,
-      default: '0'
-    },
-    imageUrl: {
-      type: String,
-      default: 'https://ossweb-img.qq.com/images/lol/web201310/skin/big84000.jpg'
-    },
-    number: {
-      type: Number,
-      default: 123456
-    },
-    account: {
-      type: String,
-      default: 'nankai123'
-    },
-    nickname: {
-      type: String,
-      default: 'nankai'
-    },
-    gender: {
-      type: String,
-      default: '男性'
-    },
-    status: {
-      type: String,
-      default: '正常'
-    },
-    role: {
-      type: String,
-      default: 'admin1'
+  data() {
+    return {
+      id: 0,
+      imageUrl: 'https://ossweb-img.qq.com/images/lol/web201310/skin/big84000.jpg',
+      number: '123456',
+      account: 'nankai123',
+      nickname: 'nankai',
+      gender: '男性',
+      status: '正常',
+      role: 'admin1',
     }
+  },
+  // props: {
+  //   // id: {
+  //   //   type: String,
+  //   //   default: '0'
+  //   // },
+  //   imageUrl: {
+  //     type: String,
+  //     default: 'https://ossweb-img.qq.com/images/lol/web201310/skin/big84000.jpg'
+  //   },
+  //   number: {
+  //     type: Number,
+  //     default: 123456
+  //   },
+  //   account: {
+  //     type: String,
+  //     default: 'nankai123'
+  //   },
+  //   nickname: {
+  //     type: String,
+  //     default: 'nankai'
+  //   },
+  //   gender: {
+  //     type: String,
+  //     default: '男性'
+  //   },
+  //   status: {
+  //     type: String,
+  //     default: '正常'
+  //   },
+  //   role: {
+  //     type: String,
+  //     default: 'admin1'
+  //   }
+  // },
+  // store,
+  methods: {
+    getDetailData() {
+       getstafflist({}).then((response) => {
+          console.log(response)
+          staff = response.data
+          this.id = staff.user.id
+          if(staff.admin_number) {
+            this.number = staff.admin_number
+            this.role = "admin1"
+          }   
+          else {
+            this.number = staff.teacher_number
+            this.role = "teacher"
+          }
+          this.account = staff.user.username
+          this.nickname = staff.user.name
+          if(staff.user.gender.name == 'male')
+            this.sex = "男性"
+          if(staff.user.gender.name == 'female')
+            this.sex = "女性"
+          if(staff.user.user_status.name == 'normal')
+            this.state = '正常'
+          if(staff.user.user_status.name == 'abnormal')
+            this.state = '异常'
+          if(staff.user.user_status.name == 'close')
+            this.state = '关闭'
+        }).catch((error) => {
+          console.log(error)
+        })
+    }
+
+  },
+  mounted() {
+    //console.log(this.$route.query)
+    //console.log(store.state.list)
+    this.getDetailData()
   }
 }
 </script>
