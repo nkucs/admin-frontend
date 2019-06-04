e7bfe164d89d54f5f27c5b38ab92cba656aaf9ea
syc <syc_nku@163.com>
2019-06-05 05:00:59 +0800

    fix: modify staff page(#89)

--- src/views/staff/StaffCreation.vue
+++ src/views/staff/StaffCreation.vue
@@ -39,13 +39,13 @@
             <a-select-option value="关闭">关闭</a-select-option>
           </a-select>
         </a-form-item>
-        <a-form-item :label-col="labelCol" :wrapper-col="wrapperCol" label="角色">
+        <!-- <a-form-item :label-col="labelCol" :wrapper-col="wrapperCol" label="角色">
           <a-select class="test" style="width: 100%" v-model="staffInfo.role">
             <a-select-option value="admin1">admin1</a-select-option>
             <a-select-option value="admin2">admin2</a-select-option>
             <a-select-option value="teacher1">teacher1</a-select-option>
           </a-select>
-        </a-form-item>
+        </a-form-item> -->
       </a-form>
     </a-card>
     <div style="float: right; margin-top: 20px">
